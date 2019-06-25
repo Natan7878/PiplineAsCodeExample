2aba241d84de475eb4864c7d5c6a4b2e0cb910a5
Natan7878 <seg.natan@gmail.com>
2019-06-25 13:22:58 -0400

    Add files via upload
    testing jen

---
+++ deleteme.groovy
@@ -0,0 +1,21 @@
+
+pipline {
+    agent any
+    stages {
+        stage('Initialize') {
+            steps {
+                sh '''
+                    echo "PATH = ${PATH}"
+                    echo "M2_HOME = &{M2_HOME}"
+                    '''
+            }
+        }
+        stage('Build') {
+            steps {
+                echo 'Hwllo world'
+            }
+        }
+
+    }
+
+}
